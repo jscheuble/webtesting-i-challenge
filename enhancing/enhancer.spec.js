62c060015fc382f18443a95535edc2eafb6b4e81
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-04-22 15:07:39 -0700

    first test passed

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,17 @@
-const enhancer = require('./enhancer.js');
+const { repair, succeed, fail, get } = require("./enhancer.js");
 // test away!
+
+const item = {
+  name: "item",
+  durability: 0,
+  enhancement: 0,
+};
+
+describe("enhancer.js", () => {
+  describe("repair()", () => {
+    it("should accept an item object and restore durability to 100", () => {
+      repair(item);
+      expect(item.durability).toBe(100);
+    });
+  });
+});
